e6578ca690f03bcb573dc90110d41ad434c46733
Tejeshwar-SM <work10tejeshwar@gmail.com>
2024-06-21 00:00:27 +0530

    added color change on hover functionality, reset button

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,12 +1,34 @@
-const grid = document.querySelector('#container');
+const gridContainer = document.querySelector('.grid');
 // const gridSize = Number(prompt('What is the number of rows'))
 const gridSize = 16;
-for(let i=1; i<=16; i++) {
-    //create 16 rows
-    const cellRows = document.createElement('div');
-    
-    cellRows.classList.add('grid-cell')
-    grid.appendChild(cellRows);
-    
-    console.log(`it works ${i}`);
+
+const resetBtn = document.querySelector('.resetBtn');
+
+
+function createGrid() {
+    for(let i=0; i<gridSize; i++) {
+        for(let j=0; j<16; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('gridCell');
+
+            gridContainer.appendChild(cell);
+        }
+    }
 }
+
+createGrid();
+
+//now create a function which applies color when there is a mouse over
+//an event listener which will create a new class active inside the gridCell
+gridContainer.addEventListener('mouseover', (event) => {
+    if(event.target.matches('.gridCell')) {
+        event.target.classList.add('active');
+    }
+});
+
+resetBtn.addEventListener('click', ()=> {
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    createGrid();
+});
