804679cd90e21bd1dbb5928d80959896a27f847e
Tejeshwar-SM <work10tejeshwar@gmail.com>
2024-06-21 11:37:08 +0530

    added toggle draw feature

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,13 +1,14 @@
 const gridContainer = document.querySelector('.grid');
 // const gridSize = Number(prompt('What is the number of rows'))
-const gridSize = 16;
+const gridSize = document.querySelector('input');
+let drawing = true;
 
 const resetBtn = document.querySelector('.resetBtn');
 
-
-function createGrid() {
-    for(let i=0; i<gridSize; i++) {
-        for(let j=0; j<16; j++) {
+let grids = 16;
+function createGrid(grids) {
+    for(let i=0; i<grids; i++) {
+        for(let j=0; j<grids; j++) {
             const cell = document.createElement('div');
             cell.classList.add('gridCell');
 
@@ -18,17 +19,38 @@ function createGrid() {
 
 createGrid();
 
+//a function which enables drawing
+function enableDrawing() {
+    gridContainer.addEventListener('click', Draw);
+    drawing = true;
+    
+}
+enableDrawing();
+
+function toggleDrawing() {
+    gridContainer.addEventListener('dblclick', ()=> {
+        drawing =!drawing;
+    })
+}
+toggleDrawing();
+
 //now create a function which applies color when there is a mouse over
 //an event listener which will create a new class active inside the gridCell
-gridContainer.addEventListener('mouseover', (event) => {
-    if(event.target.matches('.gridCell')) {
-        event.target.classList.add('active');
-    }
-});
+
+function Draw() {
+    gridContainer.addEventListener('mouseover', (event) => {
+        if(event.target.matches('.gridCell') && drawing===true) {
+            event.target.classList.add('active');
+        }
+    });
+}
+
 
 resetBtn.addEventListener('click', ()=> {
     while(gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.lastChild);
     }
     createGrid();
+    drawing = false;
+    // clickGrid();
 });
